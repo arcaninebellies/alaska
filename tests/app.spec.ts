3e1cbdab7502f5277b177cbf5b11ab1dbde9447a
notblizzard <arcaninebellies@gmail.com>
2023-09-04 20:02:45 -0400

    Update workflow files

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,6 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("http://localhost:5040");
-
   await expect(page).toHaveTitle(/Alaska/);
 });
