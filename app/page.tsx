a1b78c780c546e4f57fc04f355ceffa83ae5201b
notblizzard <arcaninebellies@gmail.com>
2023-09-06 19:21:29 -0400

    Add signout button on main page

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,19 @@ export default function Home() {
       <p className="text-7xl text-transparent bg-gradient-to-r from-sky-500 to-slate-900 bg-clip-text">
         alaska
       </p>
-      {!session && (
+      {!session ? (
         <button
           className="bg-sky-500/50 p-4 rounded-lg text-white"
           onClick={() => signIn()}
         >
           Sign In
+        </button>
+      ) : (
+        <button
+          className="bg-sky-500/50 p-4 rounded-lg text-white"
+          onClick={() => signOut()}
+        >
+          Sign Out
         </button>
       )}
     </div>
