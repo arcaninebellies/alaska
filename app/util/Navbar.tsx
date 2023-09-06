68512cc9163211f462e3f8a0f015821aecd231ba
notblizzard <arcaninebellies@gmail.com>
2023-09-06 16:44:09 -0400

    Add ability to sign out

--- app/util/Navbar.tsx
+++ app/util/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -26,36 +26,51 @@ export default function Navbar() {
 
   return (
     <>
-      {user && (
-        <div className=" flex flex-row bg-slate-100 shadow text-black p-4 justify-end w-full sticky top-0">
-          <Link href="/user/settings">
-            <div className="flex flex-row justify-center items-center content-center mr-4">
-              <p className="text-lg">Settings</p>
+      <div className=" flex flex-row bg-slate-100 shadow text-black p-4 justify-end w-full sticky top-0">
+        {user ? (
+          <>
+            <div onClick={() => signOut()}>
+              <div className="flex flex-row justify-center items-center content-center mr-4">
+                <p className="text-lg">Sign Out</p>
+              </div>
             </div>
-          </Link>
-          <Link href="/post/new">
-            <div className="flex flex-row justify-center items-center content-center mr-4">
-              <p className="text-lg">Write</p>
+            <Link href="/user/settings">
+              <div className="flex flex-row justify-center items-center content-center mr-4">
+                <p className="text-lg">Settings</p>
+              </div>
+            </Link>
+            <Link href="/post/new">
+              <div className="flex flex-row justify-center items-center content-center mr-4">
+                <p className="text-lg">Write</p>
+              </div>
+            </Link>
+            <Link href="/user/articles">
+              <div className="flex flex-row justify-center items-center content-center mr-4">
+                <p className="text-lg">Articles</p>
+              </div>
+            </Link>
+            <Link href={`/user/${user.username}`}>
+              <div className="flex flex-row justify-center items-center">
+                <Image
+                  src={`/avatars/${user.avatar}`}
+                  alt={user.username}
+                  height={30}
+                  width={30}
+                  className="rounded-full"
+                />
+              </div>
+            </Link>
+          </>
+        ) : (
+          <>
+            <div onClick={() => signIn()}>
+              <div className="flex flex-row justify-center items-center content-center mr-4">
+                <p className="text-lg">Sign In</p>
+              </div>
             </div>
-          </Link>
-          <Link href="/user/articles">
-            <div className="flex flex-row justify-center items-center content-center mr-4">
-              <p className="text-lg">Articles</p>
-            </div>
-          </Link>
-          <Link href={`/user/${user.username}`}>
-            <div className="flex flex-row justify-center items-center">
-              <Image
-                src={`/avatars/${user.avatar}`}
-                alt={user.username}
-                height={30}
-                width={30}
-                className="rounded-full"
-              />
-            </div>
-          </Link>
-        </div>
-      )}
+          </>
+        )}
+      </div>
     </>
   );
 }
